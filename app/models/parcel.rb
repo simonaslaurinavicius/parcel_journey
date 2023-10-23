9e3320b3b5a888849dc27e05de1d1deae1c56350
Simonas Laurinavicius <simonas_laurinavicius@outlook.com>
2023-10-23 13:39:40 +0300

    Add status enum to the Parcel model

--- app/models/parcel.rb
+++ app/models/parcel.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class Parcel < ApplicationRecord
+  enum status: {
+    pre_shipped: 'pre_shipped',
+    in_transit: 'in_transit',
+    completed: 'completed',
+  }
 end
