c821aafd5722d433ef18d333637eeda1b8342589
Simonas Laurinavicius <simonas_laurinavicius@outlook.com>
2023-10-23 21:47:24 +0300

    Change order of recipient/sender id in the parcels table

--- db/migrate/20231023183945_add_recipient_sender_refs_to_parcels.rb
+++ db/migrate/20231023183945_add_recipient_sender_refs_to_parcels.rb
@@ -2,7 +2,7 @@
 
 class AddRecipientSenderRefsToParcels < ActiveRecord::Migration[7.0]
   def change
-    add_reference :parcels, :recipient, null: false, foreign_key: { to_table: :people }
     add_reference :parcels, :sender, null: false, foreign_key: { to_table: :people }
+    add_reference :parcels, :recipient, null: false, foreign_key: { to_table: :people }
   end
 end
