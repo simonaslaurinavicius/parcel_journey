50f83167f7699ee6f0e44f8977dca21dcf604616
Simonas Laurinavicius <simonas_laurinavicius@outlook.com>
2023-10-23 14:39:21 +0300

    Add insert your code here comments

--- spec/models/parcel_spec.rb
+++ spec/models/parcel_spec.rb
@@ -2,28 +2,56 @@
 
 RSpec.describe Parcel do
   describe '#update_status' do
+    subject # { insert your code here }
+
+    # Refactor valid transition test cases using subject, context and let variables
     it 'updates status from pre_shipped to in_transit' do
-      # insert your code here
+      parcel = Parcel.create(status: 'pre_shipped')
+
+      parcel.update_status('in_transit')
+
+      expect(parcel.status).to eq('in_transit')
     end
 
     it 'updates status from in_transit to completed' do
-      # insert your code here
+      parcel = Parcel.create(status: 'in_transit')
+
+      parcel.update_status('completed')
+
+      expect(parcel.status).to eq('completed')
     end
 
+    # Refactor invalid transition test cases using subject, context and let variables
     it 'does not update status from pre_shipped to completed' do
-      # insert your code here
+      parcel = Parcel.create(status: 'pre_shipped')
+
+      parcel.update_status('completed')
+
+      expect(parcel.status).to eq('pre_shipped')
     end
 
     it 'does not update status from completed to in_transit' do
-      # insert your code here
+      parcel = Parcel.create(status: 'completed')
+
+      parcel.update_status('in_transit')
+
+      expect(parcel.status).to eq('completed')
     end
 
     it 'does not update status from completed to pre_shipped' do
-      # insert your code here
+      parcel = Parcel.create(status: 'completed')
+
+      parcel.update_status('pre_shipped')
+
+      expect(parcel.status).to eq('completed')
     end
 
     it 'does not update status from in_transit to pre_shipped' do
-      # insert your code here
+      parcel = Parcel.create(status: 'in_transit')
+
+      parcel.update_status('pre_shipped')
+
+      expect(parcel.status).to eq('in_transit')
     end
   end
 end
