0b4e331108fce6cb2fac258250ae542ac947a345
Simonas Laurinavicius <simonas_laurinavicius@outlook.com>
2023-10-23 13:43:36 +0300

    Add status update functionality to the Parcel model

--- app/models/parcel.rb
+++ app/models/parcel.rb
@@ -6,4 +6,27 @@ class Parcel < ApplicationRecord
     in_transit: 'in_transit',
     completed: 'completed',
   }
+
+  HAPPY_PATH = {
+    'pre_shipped' => 'in_transit',
+    'in_transit' => 'completed',
+  }.freeze
+
+  def update_status(new_status)
+    return if status == new_status
+
+    update!(status: new_status) if supported_transition?(status, new_status)
+  end
+
+  private
+
+  def supported_transition?(old, new)
+    return false if final?(old)
+
+    HAPPY_PATH[old] == new
+  end
+
+  def final?(status)
+    %w(completed).include?(status)
+  end
 end
