1c010fc0c62b1b597ecb67363328a787cc4137c7
Simonas Laurinavicius <simonas_laurinavicius@outlook.com>
2023-10-23 21:36:25 +0300

    Create people table

---
+++ db/migrate/20231023183454_create_people.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreatePeople < ActiveRecord::Migration[7.0]
+  def change
+    create_table :people do |t|
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps
+    end
+  end
+end
