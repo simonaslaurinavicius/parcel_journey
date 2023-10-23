e994af79723d0955ec49779b3a1562bd3bc914d6
Simonas Laurinavicius <simonas_laurinavicius@outlook.com>
2023-10-23 14:18:05 +0300

    Add insert your code here comments

--- spec/models/parcel_spec.rb
+++ spec/models/parcel_spec.rb
@@ -1,5 +1,29 @@
 # frozen_string_literal: true
 
 RSpec.describe Parcel do
-  # insert your code here
+  describe '#update_status' do
+    it 'updates status from pre_shipped to in_transit' do
+      # insert your code here
+    end
+
+    it 'updates status from in_transit to completed' do
+      # insert your code here
+    end
+
+    it 'does not update status from pre_shipped to completed' do
+      # insert your code here
+    end
+
+    it 'does not update status from completed to in_transit' do
+      # insert your code here
+    end
+
+    it 'does not update status from completed to pre_shipped' do
+      # insert your code here
+    end
+
+    it 'does not update status from in_transit to pre_shipped' do
+      # insert your code here
+    end
+  end
 end
