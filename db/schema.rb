f7ea8a6056a9f57a5b9fe6aaee77947a61932752
Simonas Laurinavicius <simonas_laurinavicius@outlook.com>
2023-10-23 21:36:25 +0300

    Create people table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_23_103753) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_23_183454) do
   create_table "parcels", force: :cascade do |t|
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
