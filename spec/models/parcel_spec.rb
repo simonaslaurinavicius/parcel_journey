e6e98dcbfaf2a3be810db841eb934fad5c1a25ab
Simonas Laurinavicius <simonas_laurinavicius@outlook.com>
2023-10-23 17:22:42 +0300

    Add insert your code here comments

--- spec/models/parcel_spec.rb
+++ spec/models/parcel_spec.rb
@@ -4,10 +4,13 @@ RSpec.describe Parcel do
   describe '#update_status' do
     subject { Parcel.create!(status: old_status) }
 
-    # Reduce duplication by using table tests
-    context 'with transition from pre_shipped to in_transit' do
-      let(:old_status) { 'pre_shipped' }
-      let(:new_status) { 'in_transit' }
+    {
+      %w(pre_shipped in_transit) => 'in_transit',
+      %w(in_transit completed) => 'completed',
+    }.each do |transition, _expected_status|
+      context "with valid transition from #{transition.first} to #{transition.last}" do
+        let(:old_status) { transition.first }
+        let(:new_status) { transition.last }
 
         it 'updates status' do
           subject.update_status(new_status)
@@ -17,9 +20,15 @@ RSpec.describe Parcel do
       end
     end
 
-    context 'with transition from pre_shipped to completed' do
-      let(:old_status) { 'pre_shipped' }
-      let(:new_status) { 'completed' }
+    {
+      %w(pre_shipped completed) => 'pre_shipped',
+      %w(completed in_transit) => 'completed',
+      %w(completed pre_shipped) => 'completed',
+      %w(in_transit pre_shipped) => 'in_transit',
+    }.each do |transition, expected_status|
+      context "with invalid transition from #{transition.first} to #{transition.last}" do
+        let(:old_status) { transition.first }
+        let(:new_status) { transition.last }
 
         it 'does not update status' do
           # Insert your code here
