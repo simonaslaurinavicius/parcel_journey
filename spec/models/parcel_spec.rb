35332969c8e1fd9cfb56f4ef0cec9e14396bcb46
Simonas Laurinavicius <simonas_laurinavicius@outlook.com>
2023-10-24 12:49:50 +0300

    Update code to reflect the state of the workshop

--- spec/models/parcel_spec.rb
+++ spec/models/parcel_spec.rb
@@ -7,10 +7,9 @@ RSpec.describe Parcel do
     {
       %w(pre_shipped in_transit) => 'in_transit',
       %w(in_transit completed) => 'completed',
-    }.each do |transition, expected_status|
-      context "with valid transition from #{transition.first} to #{transition.last}" do
-        let(:old_status) { transition.first }
-        let(:new_status) { transition.last }
+    }.each do |(old_status, new_status), expected_status|
+      context "with valid transition from #{old_status} to #{new_status}" do
+        subject { Parcel.create!(status: old_status) }
 
         it 'updates status' do
           subject.update_status(new_status)
@@ -25,16 +24,27 @@ RSpec.describe Parcel do
       %w(completed in_transit) => 'completed',
       %w(completed pre_shipped) => 'completed',
       %w(in_transit pre_shipped) => 'in_transit',
-    }.each do |transition, expected_status|
-      context "with invalid transition from #{transition.first} to #{transition.last}" do
-        let(:old_status) { transition.first }
-        let(:new_status) { transition.last }
+    }.each do |(old_status, new_status), expected_status|
+      context "with invalid transition from #{old_status} to #{new_status}" do
+        subject { Parcel.create!(status: old_status) }
 
         it 'does not update status' do
           # Insert your code here
           subject.update_status(new_status)
 
           expect(subject.status).to eq(expected_status)
+        end
+      end
+    end
+
+    Parcel.statuses.values.each do |status|
+      context 'when status did not change' do
+        subject { Parcel.create!(status: status) }
+
+        it 'does not update status' do
+          subject.update_status(status)
+
+          expect(subject.status).to eq(status)
         end
       end
     end
